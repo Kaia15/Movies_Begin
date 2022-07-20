069bf1977d49942dbbaf0e9f92cf08b8bf1da522
Kaia15 <86872685+Kaia15@users.noreply.github.com>
2022-07-20 08:28:13 +0700

    Add files via upload

---
+++ src/Movies/Films/films.js
@@ -0,0 +1,78 @@
+const filmList = [
+    {
+        Title: "Reply 1988",
+        Year: "2015–2016",
+        imdbID: "tt5182866",
+        Type: "series",
+        Nation: "Korean",
+        Poster: "https://m.media-amazon.com/images/M/MV5BNDYwODdiOTItZTExZC00ZDZjLTkyODMtMWYxODVlMjQ0YjQxXkEyXkFqcGdeQXVyNjExNTE4MzU@._V1_SX300.jpg"
+    },
+    {   
+        Title: 'Nevertheless', 
+        Year: '2021', 
+        imdbID: 'tt14518284', 
+        Type: 'series',
+        Nation: "Korean",
+        Poster: "https://m.media-amazon.com/images/M/MV5BYTYwYmQ0YTUtMzY2Yi00ZTIyLTk3ZDctMDU5YzEwNjBmYzgzXkEyXkFqcGdeQXVyNDYzNDg2MTM@._V1_SX300.jpg"
+    },
+    {
+        Title: "Spirited Away",
+        Year: "2005",
+        imdbID: "tt10826121",
+        Type: "series",
+        Nation: "Japanese",
+        Poster: "https://i.pinimg.com/474x/0a/69/d7/0a69d78fb1d75f8ad64735cbdbe561a5.jpg"
+    },
+    {
+        Title: "Howl's moving castle",
+        Year: "2003",
+        imdbID: "tt10877391",
+        Type: "series",
+        Nation: "Japanese",
+        Poster: "https://i.pinimg.com/474x/d8/7a/0f/d87a0ff6fe21461e1bced7adacb79022.jpg"
+    },
+    {
+        Title: "Call Me by Your Name",
+        Year: "2017",
+        imdbID: "tt5726616",
+        Type: "movie",
+        Nation: "Korean",
+        Poster: "https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"
+    },
+    {
+        Title: "When the Camellia Blooms",
+        Year: "2019",
+        imdbID: "tt10826064",
+        Type: "series",
+        Nation: "Korean",
+        Poster: "https://m.media-amazon.com/images/M/MV5BZjM5ODk0MGItOWFjNS00NjUxLWEwM2EtYzBiYjAwNzE0MmE3XkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_SX300.jpg"
+    },
+    {
+        Title: "My Neighbor Totoro",
+        Year: "1988",
+        imdbID: "tt10826065",
+        Type: "series",
+        Nation: "Japanese",
+        Poster: "https://i.pinimg.com/474x/39/c9/ad/39c9ad4466a7433e66e5e4cf28ec4cff.jpg"
+    },
+    {
+        Title: "Midnight Diner",
+        Year: "2009",
+        imdbID: "tt10826123",
+        Type: "series",
+        Nation: "Japanese",
+        Poster: "https://i.pinimg.com/474x/eb/1a/45/eb1a45387d24caae93e9c5165c25fede.jpg"
+    },
+    {
+        Title: "Garden of words",
+        Year: "2001",
+        imdbID: "tt10826159",
+        Type: "series",
+        Nation: "Japanese",
+        Poster: "https://tse4.mm.bing.net/th?id=OIP.frmoOGC8PpKNUpqYGQzZBAHaEK&pid=Api&P=0"
+    },
+    
+
+]
+
+export default filmList;
